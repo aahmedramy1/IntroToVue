7b20dbc3e053b5b5758c5e3893e997c7fa362c55
Ahmed Ramy <ahmedramy.e404@gmail.com>
2022-08-07 22:14:37 +0200

    Added product component

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `        <div class="product">
+    <div class="product-image">
+        <img :src="image" alt="Socks image">
+    </div>
+    <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if = "inStock">In Stock</p>
+        <p v-else="inStock">Out of Stock</p>
+        <p>Shipping: {{shipping}}</p>
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <div 
+            v-for = "(variant, index) in variants"
+            :key = "variant.variantId" 
+            class="color-box" 
+            :style="{backgroundColor: variant.variantColor}" 
+            @mouseover = "updateProduct(index)"
+            >
+        </div>
+        <button v-on:click = "addToCart" :disabled = "!inStock" :class="{disabledButton: !inStock}">Add to Cart</button>
+        <div class="cart">
+            <p>Cart({{cart}})</p>
+        </div>
+    </div>
+</div>`,
+data() {
+    return {
         brand: "Vue Mastery",
         product: 'Socks',
         selectedVariant: 0,
@@ -20,26 +52,40 @@ var app = new Vue({
             }
         ],
         cart: 0
+    }
+} ,
+methods: {
+    addToCart() {
+        this.cart += 1
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index
-            console.log(index)
-        }
+    updateProduct(index) {
+        this.selectedVariant = index
+        console.log(index)
+    }
+},
+computed: {
+    title() {
+        return this.brand + ' ' + this.product
     },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity
+    image() {
+        return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+        return this.variants[this.selectedVariant].variantQuantity
+    },
+    shipping() {
+        if (this.premium) {
+            return "Free"
+        } else {
+            return "2.99"
         }
     }
+}
+})
 
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: false
+    }
 });
